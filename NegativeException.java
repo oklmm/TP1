302152821ea1104b2f71def8de3662123279b2fe
oklmm <32333998+oklmm@users.noreply.github.com>
2019-02-04 11:44:32 +0100

    Add files via upload

---
+++ NegativeException.java
@@ -0,0 +1,9 @@
+package Racine;
+
+public class NegativeException extends Exception{
+    private static final long serialVersionUID = -3066290066635497976L;
+
+    public NegativeException(String message) {
+        super(message);
+    }
+}
---
+++ Racine.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+/**
+ * @author adinhrelja
+ *
+ */
+package Racine;
+
+public class Racine
+{
+	private double precision;
+	Racine(double precision)
+	{
+		this.precision=precision;
+	}
+	double racine(double operande) throws NegativeException{
+		if(operande < 0) {
+			throw new NegativeException("The number is negative");
+		}
+	
+		double solution, carre, a, b;
+		a=0;
+		b=operande;
+		solution=(a+b)/2;
+		while(b-a>precision)
+		{
+			carre=solution*solution;
+			if (carre>operande) b=solution;
+			else a=solution;
+			solution=(a+b)/2;
+		}
+		return solution;
+	}
+	
+	double getPrecision() { return precision; }
+	void setPrecision(double precision) { this.precision=precision; }
+}
